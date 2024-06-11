bf98c359f21fd9e8aa7f779fb54738b15fcd97c9
txhsl <799498265@qq.com>
2024-06-11 17:32:30 +0800

    reuse some codes

--- public_key.go
+++ public_key.go
@@ -50,17 +50,11 @@ func (pk *PublicKey) Encrypt(msg *bls.PointG1) *CipherText {
 	}
 }
 
-func (pk *PublicKey) VerifySig(msg []byte, sig *Signature) bool {
-	g2 := bls.NewG2()
-	g2Hash, _ := g2.HashToCurve(msg, Domain)
-
-	pairing := bls.NewEngine()
-	e1 := pairing.AddPair(pk.pg1, g2Hash).Result()
-	e2 := pairing.AddPair(&bls.G1One, sig.pg2).Result()
-	return e1.Equal(e2)
+func (pk *PublicKey) VerifySigShare(msg []byte, sig *SignatureShare) bool {
+	return pk.VerifySig(msg, (*Signature)(sig))
 }
 
-func (pk *PublicKey) VerifySigShare(msg []byte, sig *SignatureShare) bool {
+func (pk *PublicKey) VerifySig(msg []byte, sig *Signature) bool {
 	g2 := bls.NewG2()
 	g2Hash, _ := g2.HashToCurve(msg, Domain)
 
