63b2fdba19189be367ae71a60f5ae671386bfbf7
doubiliu <lmy0930@126.com>
2024-10-30 10:00:26 +0800

    format

--- circom/circom_test.go
+++ circom/circom_test.go
@@ -1,6 +1,8 @@
 package circom
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/consensys/gnark-crypto/ecc"
 	fr_bls12381 "github.com/consensys/gnark-crypto/ecc/bls12-377/fr"
@@ -188,17 +190,19 @@ func Phase2clone(phase2 mpcsetup.Phase2) mpcsetup.Phase2 {
 
 type MixEncryptionTest[T1, S1, T2, S2 emulated.FieldParams] struct {
 	SmallR emulated.Element[S1]
-	BigR   sw_emulated.AffinePoint[T1] `gnark:",public"`
-	Pub    sw_emulated.AffinePoint[T1] `gnark:",public"`
+	BigR   sw_emulated.AffinePoint[T1] //`gnark:",public"`
+	Pub    sw_emulated.AffinePoint[T1] //`gnark:",public"`
 	RPub   sw_emulated.AffinePoint[T1]
 
 	PlainChunks  []frontend.Variable
-	Iv           [12]frontend.Variable `gnark:",public"`
-	ChunkIndex   frontend.Variable     `gnark:",public"`
-	CipherChunks []frontend.Variable   `gnark:",public"`
+	Iv           [12]frontend.Variable //`gnark:",public"`
+	ChunkIndex   frontend.Variable     //`gnark:",public"`
+	CipherChunks []frontend.Variable   //`gnark:",public"`
 
 	SmallFi emulated.Element[S2]
-	Fi      sw_emulated.AffinePoint[T2] `gnark:",public"`
+	Fi      sw_emulated.AffinePoint[T2] //`gnark:",public"`
+	//maybe can make a hash =(input1,input2.....) to reduce public input counts
+	AllHash []frontend.Variable `gnark:",public"`
 }
 
 func (c *MixEncryptionTest[T1, S1, T2, S2]) Define(api frontend.API) error {
@@ -283,7 +287,55 @@ func (c *MixEncryptionTest[T1, S1, T2, S2]) Define(api frontend.API) error {
 	for i := range plainBits {
 		api.AssertIsEqual(plainBits[i], smallFiBits[i])
 	}
+	//check AllHash =(pub1,pub2.....)
+	rawBigR := cr.MarshalG1(c.BigR)
+	rawPub := cr.MarshalG1(c.Pub)
+	rawFi := cr2.MarshalG1(c.Fi)
+	nbBits1 := 8 * ((fp.Modulus().BitLen() + 7) / 8)
+	nbBits2 := 8 * ((fr_bls12381.Modulus().BitLen() + 7) / 8)
+	rawBigR_u8 := VariabletoU8(rawBigR, nbBits1)
+	rawPub_U8 := VariabletoU8(rawPub, nbBits1)
+	rawFi_u8 := VariabletoU8(rawFi, nbBits2)
+	length := len(rawBigR_u8) + len(rawPub_U8) + len(rawFi_u8) + len(c.Iv) + 1 + len(c.CipherChunks)
+	rawPubInputs := make([]uints.U8, length)
+
+	for i := range rawBigR_u8 {
+		rawPubInputs[i] = rawBigR_u8[i]
+	}
+	for i := range rawPub_U8 {
+		rawPubInputs[len(rawBigR_u8)+i] = rawPub_U8[i]
+	}
+	for i := range rawFi_u8 {
+		rawPubInputs[len(rawBigR_u8)+len(rawPub_U8)+i] = rawFi_u8[i]
+	}
+
+	for i := range c.Iv {
+		rawPubInputs[len(rawBigR_u8)+len(rawPub_U8)+len(rawFi_u8)+i] = uints.U8{Val: c.Iv[i]}
+	}
+	rawPubInputs[len(rawBigR_u8)+len(rawPub_U8)+len(rawFi_u8)+len(c.Iv)] = uints.U8{Val: c.ChunkIndex}
+	for i := range c.CipherChunks {
+		rawPubInputs[len(rawBigR_u8)+len(rawPub_U8)+len(rawFi_u8)+len(c.Iv)+1+i] = uints.U8{Val: c.CipherChunks[i]}
+	}
+
+	//rawPubInputs := append(append(append(append(append(rawBigR, rawPub), rawFi), c.Iv), c.ChunkIndex), c.CipherChunks)
+
+	mc, err := zksha3.New256(api)
+	mc.Write(rawPubInputs)
+	result := mc.Sum()
+
+	for i := 0; i < len(result); i++ {
+		api.AssertIsEqual(result[i].Val, c.AllHash[i])
+	}
+
 	return nil
+}
+
+func VariabletoU8(in []frontend.Variable, nbBits int) []uints.U8 {
+	out := make([]uints.U8, 2*nbBits)
+	for i := 0; i < len(out); i++ {
+		out[i] = uints.U8{Val: in[i]}
+	}
+	return out
 }
 
 func TestMixEncryption(t *testing.T) {
@@ -354,10 +406,48 @@ func TestMixEncryption(t *testing.T) {
 	_, _, g12381, _ := bls12381.Generators()
 	var Fi bls12381.G1Affine
 	Fi.ScalarMultiplication(&g12381, SmallFi)
+
+	//get allHash
+	rawBigR := BigR.RawBytes()
+	RawBigR := make([]byte, 2*fr_secp.Bytes*8)
+	for i := 0; i < nbBytes; i++ {
+		for j := 0; j < 8; j++ {
+			RawBigR[i*8+j] = (rawBigR[i] >> (7 - j)) & 1
+		}
+	}
+	rawPub := Pub.RawBytes()
+	RawPub := make([]byte, 2*fr_secp.Bytes*8)
+	for i := 0; i < nbBytes; i++ {
+		for j := 0; j < 8; j++ {
+			RawPub[i*8+j] = (rawPub[i] >> (7 - j)) & 1
+		}
+	}
+	rawFi := Fi.RawBytes()
+	RawFi := make([]byte, 2*fr_secp.Bytes*8)
+	for i := 0; i < nbBytes; i++ {
+		for j := 0; j < 8; j++ {
+			RawFi[i*8+j] = (rawFi[i] >> (7 - j)) & 1
+		}
+	}
+	temp := append(append(append(append(append(RawBigR, RawPub...), RawFi...), nonce...), 2), ciphertext...)
+
+	hasher2 := sha3.New256()
+	hasher2.Write(temp)
+	raw_allHash := hasher2.Sum(nil)
+	allHash := make([]frontend.Variable, len(raw_allHash))
+	for i := 0; i < len(allHash); i++ {
+		allHash[i] = raw_allHash[i]
+	}
+	/*	goMimc := hash.MIMC_BN254.New()
+		for i := 0; i < 10; i++ {
+			goMimc.Write(temp)
+		}
+		allHash := goMimc.Sum(nil)*/
 	//proof
 	circuit := MixEncryptionTest[emulated.Secp256k1Fp, emulated.Secp256k1Fr, emulated.BLS12381Fp, emulated.BLS12381Fr]{
 		PlainChunks:  make([]frontend.Variable, len(PlainChunksBytes)),
 		CipherChunks: make([]frontend.Variable, len(Ciphertext_bytes)),
+		AllHash:      make([]frontend.Variable, len(allHash)),
 	}
 	witness := MixEncryptionTest[emulated.Secp256k1Fp, emulated.Secp256k1Fr, emulated.BLS12381Fp, emulated.BLS12381Fr]{
 		SmallR: emulated.ValueOf[emulated.Secp256k1Fr](SmallR),
@@ -383,6 +473,7 @@ func TestMixEncryption(t *testing.T) {
 			X: emulated.ValueOf[emulated.BLS12381Fp](Fi.X),
 			Y: emulated.ValueOf[emulated.BLS12381Fp](Fi.Y),
 		},
+		AllHash: allHash,
 	}
 	err = test.IsSolved(&circuit, &witness, ecc.BN254.ScalarField())
 	assert := test.NewAssert(t)
@@ -457,18 +548,51 @@ func TestMixEncryptionByMPC(t *testing.T) {
 	_, _, g12381, _ := bls12381.Generators()
 	var Fi bls12381.G1Affine
 	Fi.ScalarMultiplication(&g12381, SmallFi)
+
+	//get allHash
+	rawBigR := BigR.RawBytes()
+	RawBigR := make([]byte, 2*fr_secp.Bytes*8)
+	for i := 0; i < nbBytes; i++ {
+		for j := 0; j < 8; j++ {
+			RawBigR[i*8+j] = (rawBigR[i] >> (7 - j)) & 1
+		}
+	}
+	rawPub := Pub.RawBytes()
+	RawPub := make([]byte, 2*fr_secp.Bytes*8)
+	for i := 0; i < nbBytes; i++ {
+		for j := 0; j < 8; j++ {
+			RawPub[i*8+j] = (rawPub[i] >> (7 - j)) & 1
+		}
+	}
+	rawFi := Fi.RawBytes()
+	RawFi := make([]byte, 2*fr_secp.Bytes*8)
+	for i := 0; i < nbBytes; i++ {
+		for j := 0; j < 8; j++ {
+			RawFi[i*8+j] = (rawFi[i] >> (7 - j)) & 1
+		}
+	}
+	temp := append(append(append(append(append(RawBigR, RawPub...), RawFi...), nonce...), 2), ciphertext...)
+
+	hasher2 := sha3.New256()
+	hasher2.Write(temp)
+	raw_allHash := hasher2.Sum(nil)
+	allHash := make([]frontend.Variable, len(raw_allHash))
+	for i := 0; i < len(allHash); i++ {
+		allHash[i] = raw_allHash[i]
+	}
 	//proof
 	circuit := MixEncryptionTest[emulated.Secp256k1Fp, emulated.Secp256k1Fr, emulated.BLS12381Fp, emulated.BLS12381Fr]{
 		PlainChunks:  make([]frontend.Variable, len(PlainChunksBytes)),
 		CipherChunks: make([]frontend.Variable, len(Ciphertext_bytes)),
+		AllHash:      make([]frontend.Variable, len(allHash)),
 	}
 	css, err := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &circuit)
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
 	//init,2ways: way1 make a new mpc, way2 from a existed mpc
-	pk, vk, _ := doMPCSetUp(css)
-	//pk, vk, _ := getFromExistedMPCSetUp(css)
+	//pk, vk, _ := doMPCSetUp(css)
+	pk, vk, _ := getFromExistedMPCSetUp(css)
 	// 1. One time setup
 	err = groth16.Setup(css.(*cs.R1CS), &pk, &vk)
 	if err != nil {
@@ -498,6 +622,7 @@ func TestMixEncryptionByMPC(t *testing.T) {
 			X: emulated.ValueOf[emulated.BLS12381Fp](Fi.X),
 			Y: emulated.ValueOf[emulated.BLS12381Fp](Fi.Y),
 		},
+		AllHash: allHash,
 	}
 	//compute witness
 	witness, err := frontend.NewWitness(assignment, ecc.BN254.ScalarField())
@@ -518,10 +643,46 @@ func TestMixEncryptionByMPC(t *testing.T) {
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
+	//print proof public msg
+	// Save publicWitness
+	schema, _ := frontend.NewSchema(assignment)
+	ret, _ := publicWitness.ToJSON(schema)
+	var b bytes.Buffer
+	json.Indent(&b, ret, "", "\t")
+	t.Logf(b.String())
+	// Save proof
+	t.Logf("proof :,key:%x", proof.MarshalSolidity())
 	//export solidity contract
 	contract, err := os.Create("verify.sol")
 	err = vk.ExportSolidity(contract)
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
+	// solidity contract inputs
+	proofBytes := proof.MarshalSolidity()
+	fpSize := 4 * 8
+	var prf [8]*big.Int
+	// proof.Ar, proof.Bs, proof.Krs
+	t.Logf("printf proof")
+	for i := 0; i < 8; i++ {
+		prf[i] = new(big.Int).SetBytes(proofBytes[fpSize*i : fpSize*(i+1)])
+		t.Logf("proof:" + prf[i].String())
+	}
+
+	/*	var commitments [{{mul 2 .NbCommitments}}]*big.Int
+		var commitmentPok [2]*big.Int
+
+		// commitments
+		t.Logf("printf commitments")
+		for i := 0; i < 2*commitmentCount; i++ {
+		commitments[i] = new(big.Int).SetBytes(proofBytes[fpSize*8+4+i*fpSize : fpSize*8+4+(i+1)*fpSize])
+		t.Logf("commitments:"+commitments[i])
+		}
+		// commitmentPok
+		commitmentPok[0] = new(big.Int).SetBytes(proofBytes[fpSize*8+4+2*commitmentCount*fpSize : fpSize*8+4+2*commitmentCount*fpSize+fpSize])
+		commitmentPok[1] = new(big.Int).SetBytes(proofBytes[fpSize*8+4+2*commitmentCount*fpSize+fpSize : fpSize*8+4+2*commitmentCount*fpSize+2*fpSize])
+		t.Logf("printf commitmentPok")
+		t.Logf("commitmentPok 0:"+commitmentPok[0] )
+		t.Logf("commitmentPok 1:"+commitmentPok[1] )*/
+
 }
